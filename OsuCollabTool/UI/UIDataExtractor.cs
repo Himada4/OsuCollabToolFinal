3480528c162d72fd59bd69405ab6a7639369fbc2
Himada <Himanoryunosuke@gmail.com>
2021-03-29 21:20:35 +0200

    UI related code

--- OsuCollabTool/UI/UIDataExtractor.cs
+++ OsuCollabTool/UI/UIDataExtractor.cs
@@ -45,60 +45,77 @@ namespace OsuCollabTool.UI
 
         public void Refresh()
         {
-            StreamReader sr = new StreamReader("UIData.txt");
-            var line = sr.ReadLine();
-            List<string> list = new List<string>();
-            int itemData = 1;
-            int i = 0;
-            while (line != null)
+            try
             {
-                switch (i)
+                StreamReader sr = new StreamReader("UIData.txt");
+                var line = sr.ReadLine();
+                List<string> list = new List<string>();
+                int itemData = 1;
+                int i = 0;
+                while (line != null)
                 {
-                    case 0:
-                        string[] son = line.Split('=');
-                        songFolder = son[itemData];
-                        break;
+                    switch (i)
+                    {
+                        case 0:
+                            string[] son = line.Split('=');
+                            songFolder = son[itemData];
+                            break;
 
-                    case 1:
-                        string[] the = line.Split('=');
-                        string main = the[itemData];
-                        string[] colData = main.Split(';');
-                        Color[] sonRes = new Color[3];
-                        for (int j = 0; j < colData.Length; j = j + 1)
-                        {
-                            string[] sepColor = colData[j].Split(',');
+                        case 1:
+                            string[] the = line.Split('=');
+                            string main = the[itemData];
+                            string[] colData = main.Split(';');
+                            Color[] sonRes = new Color[3];
+                            for (int j = 0; j < colData.Length; j = j + 1)
+                            {
+                                string[] sepColor = colData[j].Split(',');
 
-                            Color col = Color.FromArgb(Convert.ToInt32(sepColor[0]), Convert.ToInt32(sepColor[1]), Convert.ToInt32(sepColor[2]));
-                            sonRes[j] = col;
-                        }
+                                Color col = Color.FromArgb(Convert.ToInt32(sepColor[0]), Convert.ToInt32(sepColor[1]), Convert.ToInt32(sepColor[2]));
+                                sonRes[j] = col;
+                            }
 
-                        theme = sonRes;
-                        break;
+                            theme = sonRes;
+                            break;
 
-                    case 2:
-                        string[] but = line.Split('=');
-                        string split = but[itemData];
-                        string[] butRes = split.Split(',');
-                        buttonOrder = new int[] { Convert.ToInt32(butRes[0]), Convert.ToInt32(butRes[1]), Convert.ToInt32(butRes[2]), Convert.ToInt32(butRes[3]) };
-                        break;
+                        case 2:
+                            string[] but = line.Split('=');
+                            string split = but[itemData];
+                            string[] butRes = split.Split(',');
+                            buttonOrder = new int[] { Convert.ToInt32(butRes[0]), Convert.ToInt32(butRes[1]), Convert.ToInt32(butRes[2]), Convert.ToInt32(butRes[3]) };
+                            break;
 
-                    case 3:
-                        string[] fold = line.Split('=');
-                        currentFolder = fold[itemData];
-                        break;
+                        case 3:
+                            string[] fold = line.Split('=');
+                            currentFolder = fold[itemData];
+                            break;
 
-                    case 4:
-                        string[] osu = line.Split('=');
-                        currentOsu = osu[itemData];
-                        break;
+                        case 4:
+                            string[] osu = line.Split('=');
+                            currentOsu = osu[itemData];
+                            break;
+                    }
+
+                    i = i + 1;
+                    list.Add(line);
+                    line = sr.ReadLine();
                 }
 
-                i = i + 1;
-                list.Add(line);
-                line = sr.ReadLine();
+                sr.Close();
             }
+            catch
+            {
+                using (StreamWriter sw = new StreamWriter("UIData.txt", false))
+                {
+                    sw.WriteLine($"SongsFolder=");
+                    sw.WriteLine($"Theme=34,34,34;46,209,172;192,192,192");
+                    sw.WriteLine($"ButtonOrder=3,1,2,4");
+                    sw.WriteLine($"CurrentFolder=");
+                    sw.WriteLine($"CurrentOsu=");
+                    sw.Close();
+                }
 
-            sr.Close();
+                Refresh();
+            }
         }
     }
 }
